b2cd771d16c63d3193e5b5b7bdb127a8a93c0ca4
Apoorva Chikara <apoorvac@damcogroup.com>
2021-07-12 22:27:27 +0530

    Weather API - used express simpler server to serve the API

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+const axios = require('axios'); 
+
+/* GET home page. */
+
+router.get('/location/search/', async (req, res, next) => {
+    console.log(req.query);
+        const { query } = req.query;
+        const { data } = await axios(`https://www.metaweather.com/api/location/search/?query=${query}`);
+        res.send(data);
+});
+
+router.get('/location/:locationID', async (req, res, next) => {
+  
+    const locationID = req.params.locationID;
+    const { data } = await axios(`https://www.metaweather.com/api/location/${locationID}/`);
+    res.send(data);
+});
+
+
+
+
+
+module.exports = router;
